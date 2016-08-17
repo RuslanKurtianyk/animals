061ce462068d72841cc44a933b9e4ff386bf60bc
Ruslan <rkurtjanyk@razomsolution.com>
2016-08-17 20:03:49 +0000

    add html markup, angular, task for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,32 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-      sass: {                              // Task
-        dist: {                            // Target
-          options: {                       // Target options
+      sass: {                              
+        dist: {                            
+          options: {                       
             style: 'expanded'
           },
-          files: {                         // Dictionary of files
-            'assets/css/style.css': 'assets/sass/style.scss',       // 'destination': 'source'
+          files: {                         
+            'assets/css/style.css': 'assets/sass/style.scss', 
           }
         }
-      }
+      },
+      /* Watcher for babel task */
+        watch: {
+            css: {
+                files: 'assets/sass/style.scss',
+                tasks: ['sass'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-sass');
-
+  grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['watch']);
 
 };
